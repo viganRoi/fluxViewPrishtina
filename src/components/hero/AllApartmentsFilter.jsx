d6d26db17d83d2cb00e652d4c1ff1c504c663b83
Diarduriqi <diar.roitiv@gmail.com>
2025-06-26 10:28:25 +0200

    update all apartments mobile

--- src/components/hero/AllApartmentsFilter.jsx
+++ src/components/hero/AllApartmentsFilter.jsx
@@ -52,72 +52,80 @@ const AllApartmentsFilter = ({ setFilterState, available }) => {
   };
 
   return (
-    <div className="w-full h-full py-32 gap-32 flex flex-col items-center justify-center bg-black border-b border-brand">
+    <div className="w-full h-full py-20 md:py-32 px-4 md:px-0 gap-4 md:gap-32 flex flex-col items-center justify-center bg-black border-b border-brand overflow-x-hidden">
       <div className="w-11/12 h-full text-white flex flex-col justify-between items-start gap-4 border-b border-white pb-12">
-        <h1 className="font-bold text-4xl">Filtro <span className="text-brand">Apartmentet</span></h1>
+        <h1 className="font-bold text-4xl">
+          Filtro <span className="text-brand">Apartmentet</span>
+        </h1>
       </div>
       <div className="w-11/12 h-full text-gold flex flex-col justify-between items-center gap-4">
-        <div className="w-full grid grid-cols-1 md:grid-cols-4 gap-20">
+        <div className="w-full grid grid-cols-1 md:grid-cols-4 gap-10 md:gap-20">
           <div className="w-full flex flex-col items-start gap-1 md:gap-4">
             <h1 className="text-lg text-white font-semibold">Objekti</h1>
             <div className="w-full flex gap-4 justify-start">
               <button
                 name="a"
                 onClick={handleBuildingChange}
-                className={`px-4 py-2 rounded-full border border-gold ${buildingFilter.includes("a")
-                  ? "bg-brand text-black"
-                  : "bg-transparent text-white"
-                  }`}
+                className={`px-4 py-2 rounded-full border border-gold ${
+                  buildingFilter.includes("a")
+                    ? "bg-brand text-black"
+                    : "bg-transparent text-white"
+                }`}
               >
                 A
               </button>
               <button
                 name="b"
                 onClick={handleBuildingChange}
-                className={`px-4 py-2 rounded-full border border-gold ${buildingFilter.includes("b")
-                  ? "bg-brand text-black"
-                  : "bg-transparent text-white"
-                  }`}
+                className={`px-4 py-2 rounded-full border border-gold ${
+                  buildingFilter.includes("b")
+                    ? "bg-brand text-black"
+                    : "bg-transparent text-white"
+                }`}
               >
                 B
               </button>
               <button
                 name="c"
                 onClick={handleBuildingChange}
-                className={`px-4 py-2 rounded-full border border-gold ${buildingFilter.includes("c")
-                  ? "bg-brand text-black"
-                  : "bg-transparent text-white"
-                  }`}
+                className={`px-4 py-2 rounded-full border border-gold ${
+                  buildingFilter.includes("c")
+                    ? "bg-brand text-black"
+                    : "bg-transparent text-white"
+                }`}
               >
                 C
               </button>
               <button
                 name="d"
                 onClick={handleBuildingChange}
-                className={`px-4 py-2 rounded-full border border-gold ${buildingFilter.includes("d")
-                  ? "bg-brand text-black"
-                  : "bg-transparent text-white"
-                  }`}
+                className={`px-4 py-2 rounded-full border border-gold ${
+                  buildingFilter.includes("d")
+                    ? "bg-brand text-black"
+                    : "bg-transparent text-white"
+                }`}
               >
                 D
               </button>
               <button
                 name="e"
                 onClick={handleBuildingChange}
-                className={`px-4 py-2 rounded-full border border-gold ${buildingFilter.includes("e")
-                  ? "bg-brand text-black"
-                  : "bg-transparent text-white"
-                  }`}
+                className={`px-4 py-2 rounded-full border border-gold ${
+                  buildingFilter.includes("e")
+                    ? "bg-brand text-black"
+                    : "bg-transparent text-white"
+                }`}
               >
                 E
               </button>
               <button
                 name="f"
                 onClick={handleBuildingChange}
-                className={`px-4 py-2 rounded-full border border-gold ${buildingFilter.includes("f")
-                  ? "bg-brand text-black"
-                  : "bg-transparent text-white"
-                  }`}
+                className={`px-4 py-2 rounded-full border border-gold ${
+                  buildingFilter.includes("f")
+                    ? "bg-brand text-black"
+                    : "bg-transparent text-white"
+                }`}
               >
                 F
               </button>
@@ -129,40 +137,44 @@ const AllApartmentsFilter = ({ setFilterState, available }) => {
               <button
                 name="1"
                 onClick={handleRoomChange}
-                className={`px-4 py-2 rounded-full border border-gold ${roomFilter.includes("1")
-                  ? "bg-brand text-black"
-                  : "bg-transparent text-white"
-                  }`}
+                className={`px-4 py-2 rounded-full border border-gold ${
+                  roomFilter.includes("1")
+                    ? "bg-brand text-black"
+                    : "bg-transparent text-white"
+                }`}
               >
                 1 + 1
               </button>
               <button
                 name="2"
                 onClick={handleRoomChange}
-                className={`px-4 py-2 rounded-full border border-gold ${roomFilter.includes("2")
-                  ? "bg-brand text-black"
-                  : "bg-transparent text-white"
-                  }`}
+                className={`px-4 py-2 rounded-full border border-gold ${
+                  roomFilter.includes("2")
+                    ? "bg-brand text-black"
+                    : "bg-transparent text-white"
+                }`}
               >
                 2 + 1
               </button>
               <button
                 name="3"
                 onClick={handleRoomChange}
-                className={`px-4 py-2 rounded-full border border-gold ${roomFilter.includes("3")
-                  ? "bg-brand text-black"
-                  : "bg-transparent text-white"
-                  }`}
+                className={`px-4 py-2 rounded-full border border-gold ${
+                  roomFilter.includes("3")
+                    ? "bg-brand text-black"
+                    : "bg-transparent text-white"
+                }`}
               >
                 3 + 1
               </button>
               <button
                 name="penthouse"
                 onClick={handleRoomChange}
-                className={`px-4 py-2 rounded-full border border-gold ${roomFilter.includes("penthouse")
-                  ? "bg-brand text-black"
-                  : "bg-transparent text-white"
-                  }`}
+                className={`px-4 py-2 rounded-full border border-gold ${
+                  roomFilter.includes("penthouse")
+                    ? "bg-brand text-black"
+                    : "bg-transparent text-white"
+                }`}
               >
                 Penthouse
               </button>
@@ -182,19 +194,33 @@ const AllApartmentsFilter = ({ setFilterState, available }) => {
                   max={maxSquare}
                   color="var(--brand-color)"
                   sx={{
-                    color: "var(--brand-color)",
-                    height: "1px",
-                    width: "100%",
+                    color: "var(--brand-color)", // This affects the track and rail color
+                    height: 2,
+                    "& .MuiSlider-thumb": {
+                      height: 12,
+                      width: 20,
+                      backgroundColor: "#fff",
+                      border: "1px solid var(--brand-color)",
+                      "&:hover, &.Mui-focusVisible, &.Mui-active": {
+                        boxShadow: "0 0 0 6px rgba(0,0,0,0.1)",
+                      },
+                    },
+                    "& .MuiSlider-track": {
+                      backgroundColor: "var(--brand-color)",
+                    },
+                    "& .MuiSlider-rail": {
+                      backgroundColor: "#ccc", // optional, for contrast
+                    },
                   }}
                 />
               </div>
               <div className="flex items-center gap-4 w-full">
-              <p className="text-lg py-1 px-8 text-white border border-gold rounded-full">
-                {squareFilter.startVal} m<sup>2</sup>
-              </p>
-              <p className="text-lg py-1 px-8 text-white border border-gold rounded-full">
-                {squareFilter.endVal} m<sup>2</sup>
-              </p>
+                <p className="text-lg py-1 px-8 text-white border border-gold rounded-full">
+                  {squareFilter.startVal} m<sup>2</sup>
+                </p>
+                <p className="text-lg py-1 px-8 text-white border border-gold rounded-full">
+                  {squareFilter.endVal} m<sup>2</sup>
+                </p>
               </div>
             </div>
           </div>
@@ -212,19 +238,33 @@ const AllApartmentsFilter = ({ setFilterState, available }) => {
                   max={maxFloor}
                   color="var(--brand-color)"
                   sx={{
-                    color: "var(--brand-color)",
-                    height: "1px",
-                    width: "100%",
+                    color: "var(--brand-color)", // This affects the track and rail color
+                    height: 2,
+                    "& .MuiSlider-thumb": {
+                      height: 12,
+                      width: 20,
+                      backgroundColor: "#fff",
+                      border: "1px solid var(--brand-color)",
+                      "&:hover, &.Mui-focusVisible, &.Mui-active": {
+                        boxShadow: "0 0 0 6px rgba(0,0,0,0.1)",
+                      },
+                    },
+                    "& .MuiSlider-track": {
+                      backgroundColor: "var(--brand-color)",
+                    },
+                    "& .MuiSlider-rail": {
+                      backgroundColor: "#ccc", // optional, for contrast
+                    },
                   }}
                 />
               </div>
               <div className="flex items-center gap-4 w-full">
-              <p className="text-lg py-1 px-8 text-white border border-gold rounded-full">
-                {getFloorLabel(floorFilter.startVal)}
-              </p>
-              <p className="text-lg py-1 px-8 text-white border border-gold rounded-full">
-                {getFloorLabel(floorFilter.endVal)}
-              </p>
+                <p className="text-lg py-1 px-8 text-white border border-gold rounded-full">
+                  {getFloorLabel(floorFilter.startVal)}
+                </p>
+                <p className="text-lg py-1 px-8 text-white border border-gold rounded-full">
+                  {getFloorLabel(floorFilter.endVal)}
+                </p>
               </div>
             </div>
           </div>
