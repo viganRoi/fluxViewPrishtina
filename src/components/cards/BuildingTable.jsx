08e0278f18b9af4b0c189c9e823c0a1d2b953881
Diarduriqi <diar.roitiv@gmail.com>
2025-07-23 09:59:41 +0200

    update

--- src/components/cards/BuildingTable.jsx
+++ src/components/cards/BuildingTable.jsx
@@ -9,7 +9,7 @@ const BuildingTable = ({ apartments }) => {
       <div className="w-full px-12 h-full flex items-center justify-center">
         <div className="h-full w-full overflow-x-auto sm:overflow-y-auto">
           <table className="min-w-full table-auto text-xs sm:text-sm text-left border-separate border-spacing-0">
-            <thead className="bg-primary h-10 sm:h-12 text-white uppercase sticky top-0 z-10">
+            <thead className="bg-primary h-10 sm:h-12 text-black uppercase sticky top-0 z-10">
               <tr>
                 <th className="px-4 sm:px-6 py-2 sm:py-3 font-medium">
                   Apartment
@@ -28,8 +28,8 @@ const BuildingTable = ({ apartments }) => {
                   <tr
                     key={apartment.id}
                     className={`${
-                      i % 2 === 0 ? "bg-black" : "bg-gray-300"
-                    } h-10 sm:h-12 w-full text-primary cursor-pointer ${
+                      i % 2 === 0 ? "bg-black" : "bg-[#3a3a3a]/50"
+                    } h-10 sm:h-12 w-full text-white cursor-pointer ${
                       apartment.isSold ? "bg-red-500" : ""
                     } ${apartment.isReserved ? "bg-orange-400" : ""}`}
                     onClick={() => navigate(`/apartments/${apartment.id}`)}
--- src/components/hero/SingleApartment.jsx
+++ src/components/hero/SingleApartment.jsx
@@ -21,6 +21,7 @@ const SingleApartment = () => {
   const isSmallDev = window.innerWidth < 700;
   const [selectedTab, setSelectedTab] = useState("2d");
   const [isPriceCardVisible, setIsPriceCardVisible] = useState(false);
+  const [isHeartActive, setIsHeartActive] = useState(false);
 
   const apartment = useSelector(getApartmentDetailModalData);
   const wishListItemCount = useSelector(getWishlistCount);
@@ -191,12 +192,30 @@ const SingleApartment = () => {
                     )}
                   </div>
                 </div>
-                <button className="">
-                  <img
-                    className="w-[35px] md:w-[45px]"
-                    src="/assets/icons/heart.svg"
-                    alt=""
-                  />
+                <button
+                  className=""
+                  onClick={() => setIsHeartActive(!isHeartActive)}
+                >
+                  <svg
+                    width="50"
+                    height="50"
+                    viewBox="0 0 50 50"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle
+                      cx="25"
+                      cy="25"
+                      r="24.5"
+                      fill="white"
+                      stroke="#E9E9E9"
+                    />
+                    <path
+                      d="M20.8156 17.5616C21.5532 17.436 22.3095 17.4774 23.0289 17.6829C23.7483 17.8885 24.4124 18.2528 24.9723 18.7491L25.0031 18.7766L25.0314 18.7516C25.5658 18.2827 26.194 17.9331 26.8741 17.7262C27.5542 17.5193 28.2707 17.4598 28.9756 17.5516L29.1806 17.5816C30.0692 17.7351 30.8998 18.1259 31.5843 18.7129C32.2689 19.2998 32.782 20.0609 33.0693 20.9157C33.3566 21.7704 33.4074 22.6869 33.2164 23.5682C33.0253 24.4495 32.5995 25.2626 31.9839 25.9216L31.8339 26.0758L31.7939 26.11L25.5856 32.2591C25.4423 32.4009 25.2525 32.486 25.0513 32.4986C24.8502 32.5111 24.6512 32.4503 24.4914 32.3275L24.4131 32.2591L18.1689 26.0741C17.5075 25.4305 17.037 24.6164 16.8097 23.7219C16.5825 22.8273 16.6072 21.8874 16.8812 21.0061C17.1552 20.1247 17.6678 19.3365 18.3622 18.7285C19.0567 18.1206 19.9058 17.7167 20.8156 17.5616Z"
+                      fill={isHeartActive ? "red" : "transparent"}
+                      stroke={isHeartActive ? "red" : "black"}
+                    />
+                  </svg>
                 </button>
               </div>
             </div>
@@ -219,7 +238,7 @@ const SingleApartment = () => {
                     parseFloat(apartment.square) +
                     parseFloat(apartment.balconySquare)
                   ).toFixed(2)}{" "}
-                  <sup>2</sup>{" "}
+                  m<sup>2</sup>{" "}
                 </h1>
               </div>
               <div className="w-full"> </div>
