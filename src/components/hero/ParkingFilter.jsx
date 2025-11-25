7f7fd58e980ee8b3497c1a346028f42d0c6430ec
Diarduriqi <diar.roitiv@gmail.com>
2025-11-25 15:59:09 +0100

    update parking filter

--- src/components/hero/ParkingFilter.jsx
+++ src/components/hero/ParkingFilter.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const ParkingFilter = ({
   onFilterChange,
@@ -6,6 +7,7 @@ const ParkingFilter = ({
   parkingNumber,
 }) => {
   const [selectedType, setSelectedType] = useState([]);
+  const { id } = useParams();
 
   // const handleTypeChange = (type) => {
   //   setSelectedType((prev) =>
@@ -30,9 +32,9 @@ const ParkingFilter = ({
   };
 
   const mappingParkingNumberToFloor = {
-    "1": "Bodrumi",
-    "2": "Suterreni 1",
-    "3": "Suterreni 2",
+    1: id === "n1" ? "Bodrumi" : "Bodrumi -2",
+    2: id === "n1" ? "Suterreni 1" : "Bodrumi -1",
+    3: "Suterreni 2",
   };
 
   return (
@@ -49,10 +51,11 @@ const ParkingFilter = ({
                 className="hidden"
               />
               <div
-                className={`w-5 h-5 border rounded-md ${selectedType.includes("Parkingjet")
-                  ? "bg-[var(--brand-color)]"
-                  : "bg-white"
-                  }`}
+                className={`w-5 h-5 border rounded-md ${
+                  selectedType.includes("Parkingjet")
+                    ? "bg-[var(--brand-color)]"
+                    : "bg-white"
+                }`}
               />
               <span className="montserrat">Parkingjet</span>
             </label>
@@ -64,10 +67,11 @@ const ParkingFilter = ({
                 className="hidden"
               />
               <div
-                className={`w-5 h-5 border rounded-md ${selectedType.includes("Depo")
-                  ? "bg-[var(--brand-color)]"
-                  : "bg-white"
-                  }`}
+                className={`w-5 h-5 border rounded-md ${
+                  selectedType.includes("Depo")
+                    ? "bg-[var(--brand-color)]"
+                    : "bg-white"
+                }`}
               />
               <span className="montserrat">Depo</span>
             </label>
@@ -76,20 +80,24 @@ const ParkingFilter = ({
         <div className="flex flex-col items-start">
           <h3 className="text-base font-semibold montserrat mb-2">Kati</h3>
           <div className="flex space-x-2">
-            {Object.keys(mappingParkingNumberToFloor).map((floor) => (
-              <button
-                key={floor}
-                className={`px-4 py-2 border rounded-md ${String(parkingNumber) === floor
-                  ? "bg-[var(--brand2-color)] text-white"
-                  : "bg-white"
+            {Object.keys(mappingParkingNumberToFloor).map((floor, inx) => {
+              if (id === "n2" && inx === 2) return null;
+              return (
+                <button
+                  key={floor}
+                  className={`px-4 py-2 border rounded-md ${
+                    String(parkingNumber) === floor
+                      ? "bg-[var(--brand2-color)] text-white"
+                      : "bg-white"
                   }`}
-                onClick={() => {
-                  onParkingNumberChange(floor);
-                }}
-              >
-                {mappingParkingNumberToFloor[floor]}
-              </button>
-            ))}
+                  onClick={() => {
+                    onParkingNumberChange(floor);
+                  }}
+                >
+                  {mappingParkingNumberToFloor[floor]}
+                </button>
+              );
+            })}
           </div>
         </div>
         <div className="flex flex-col items-start">
